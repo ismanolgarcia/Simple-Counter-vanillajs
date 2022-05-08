ede8a92312d20c606dca997208723465810a7f54
Ismanol García <dev@ismanolgarcia.com>
2022-05-08 10:25:04 -0400

    project up

---
+++ README.md
@@ -0,0 +1,23 @@
+[web]: /media/screenshot_web.png
+
+[webgif]: /media/ScreencastWeb.gif
+
+[mobile]: media/Screenshotmobile.png
+
+[mobilegif]: media/ScreencastMobile.gif
+
+
+# Simple Counter width JavaScript Vanilla
+
+## Web|Desktop
+
+![web]
+
+![webgif]
+
+
+## Web|Mobile
+
+ ![mobile] ![mobilegif]
+
+
---
+++ app.js
@@ -0,0 +1,24 @@
+let value = document.getElementById("value");
+const decrease = document.getElementById("decrease");
+const reset = document.getElementById("reset");
+const increase = document.getElementById("increase");
+let counter = 0;
+value.textContent = counter;
+
+decrease.addEventListener("click", () => {
+  counter--;
+  value.textContent = counter;
+  value.style.color = "#ff0000";
+});
+
+reset.addEventListener("click", () => {
+  counter = 0;
+  value.textContent = counter;
+  value.style.color = "#000000";
+});
+
+increase.addEventListener("click", () => {
+  counter++;
+  value.textContent = counter;
+  value.style.color = "#006400";
+});
